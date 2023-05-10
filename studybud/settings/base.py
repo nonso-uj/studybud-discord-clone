d0af5cb8a6427039ddfd77c52c4aa74160344733
Udonne Chinonso <nonso.udonne@gmail.com>
2023-05-10 23:57:26 +0100

    Update base.py

--- studybud/settings/base.py
+++ studybud/settings/base.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
