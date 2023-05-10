b7761503299cee013868943c4a640578b48fe297
Udonne Chinonso <nonso.udonne@gmail.com>
2023-05-11 00:09:01 +0100

    Update prod.py

--- studybud/settings/prod.py
+++ studybud/settings/prod.py
@@ -1,4 +1,4 @@
-import django_on_heroku
+# import django_on_heroku
 from decouple import config
 
 from .base import *
@@ -14,7 +14,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     # change
-    'studybud-discord-clone.herokuapp.com',
+    'studybud-discord-clone.onrender.com',
 ]
 
 
@@ -41,25 +41,27 @@ INSTALLED_APPS = [
 ]
 
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # AWS S3 settings
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-
-AWS_S3_OBJECT_PARAMETERS = {
-   'CacheControl': 'max-age=86400'
-}
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
-AWS_LOCATION = 'static'
+# AWS_S3_OBJECT_PARAMETERS = {
+#    'CacheControl': 'max-age=86400'
+# }
+
+
+# AWS_LOCATION = 'static'
 
 
 STATICFILES_DIRS = [
@@ -67,28 +69,25 @@ STATICFILES_DIRS = [
 ]
 
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
-AWS_QUERYSTRING_AUTH = False
+# AWS_QUERYSTRING_AUTH = False
 
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-}
+# AWS_HEADERS = {
+#     'Access-Control-Allow-Origin': '*',
+# }
 
-AWS_DEFAULT_ACL = 'public-read'
-
-
-
+# AWS_DEFAULT_ACL = 'public-read'
 
 
 
@@ -99,35 +98,38 @@ AWS_DEFAULT_ACL = 'public-read'
 
 
 
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
-# Heroku Logging
 
-LOGGING = {
-    'version': 1,
-    'dissable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineo)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging-StreamHandler',
-        },
-    },
-    'loggers': {
-        'MYAPP': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    }
-}
+
+# DEBUG_PROPAGATE_EXCEPTIONS = True
+
+# # Heroku Logging
+
+# LOGGING = {
+#     'version': 1,
+#     'dissable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineo)s] %(message)s",
+#             'datefmt': "%d/%b/%Y %H:%M:%S"
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging-StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'MYAPP': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     }
+# }
 
 
 
@@ -149,5 +151,5 @@ LOGGING = {
 # MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # django_on_heroku.settings(locals())
-django_on_heroku.settings(locals(), staticfiles=False)
+# django_on_heroku.settings(locals(), staticfiles=False)
 # del DATABASES['default']['OPTIONS']['sslmode']
